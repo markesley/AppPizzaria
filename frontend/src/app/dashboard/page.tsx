8859ea6f54e4c1b9b8ad98bb462fd098f99eecb0
markesley <markesley10@hotmail.com>
2025-01-01 18:18:45 -0300

    ajuste front

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,36 @@
+import { Orders } from './orders';
+import { api } from '@/services/api'
+import { getCookieServer } from '@/lib/cookieServer'
+import { OrderProps } from '@/lib/order.type'
+
+async function getOrders(): Promise<OrderProps[] | []>{
+  try{
+    const token = await getCookieServer();
+
+    const response = await api.get("/orders", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    return response.data || []
+
+  }catch(err){
+    console.log(err);
+    return [];
+  }
+}
+
+
+export default async function Dashboard(){
+
+  const orders = await getOrders();
+
+  console.log(orders);
+
+  return(
+    <>
+      <Orders orders={orders}/>
+    </>
+  )
+}
