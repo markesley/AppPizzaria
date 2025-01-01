5b5e215b4dad1b12d26aa1f4c811c9ca2f9172e1
markesley <markesley10@hotmail.com>
2025-01-01 18:01:59 -0300

    arquivos novos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -40,7 +40,8 @@ router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 //ROTAS --PRODUCT
-router.post('/product', isAuthenticated,upload.single('file'), new CreateProductController().handle)
+// router.post('/product', isAuthenticated,upload.single('file'), new CreateProductController().handle)
+router.post('/product', isAuthenticated, new CreateProductController().handle)
 
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,11 +3,15 @@ import 'express-async-errors';
 import cors from 'cors'
 import {router} from './routes'
 import path from 'path'
+import fileUpload from 'express-fileupload';
 
 
 const app = express();
 app.use(express.json())
 app.use(cors())
+app.use(fileUpload({
+    limits: {fileSize: 50 * 1024 * 1024}
+}))
 app.use(router)
 
 app.use(
